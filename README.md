eaeb561835436239c29b8e5a0bf01b03381484bd
Shawn.Peng <pengshawntw@gmail.com>
2023-08-01 17:34:35 +0800

    stable version 1

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # modBIN_withXML
 modify bin with location recorded by xml
+
+## Work flow :
+1. Can change target.txt to modify more step
+   1. Default BiosStartInFullRomImage + PcdFlashNvStorageVariableOffset
+   2. Rule : one line of feature-name and one line of act 
+   3. The act only suppout +/-
+   4. If act isn't correct or feature-name cannot find than it will be skiped
+
+2. Find feature-name which user set in {Platform}.xml
+   1. Now can find feature-name in tag of first layer and its text is the value we want
+   2. Now can find feature-name in value of "name" tag of forth or fifth layers and the value of the tag "value" of the same layer is what we want
+   3. ***FF*** User can input {Platfrom} or press enter to use .xml witch first finded
+
+3. Count the value and print it to double check
+
+4. Read write_data.bin which will replace 
+   1. ***FF*** User can input 1 to choice other write_data_factory_default.bin otherwise default write_data.bin
+   2. ***FF*** User can input manually to modify CT/MAC/ForceNetBoot/counter/timer otherwise default 11002200...EE00/FF..FF/1/5/2
+   3. Format : \x00\x00\..\x07 in one line
+   4. ***FF*** Support other input format
+
+5. Write date to {Platform_version}.bin
+   1. Copy {Platform_version}.bin and rename {Platform_version}_ori.bin then progrm will modify in {Platform_version}.bin
+
+## Installation
+1. Clone the repository: ```git clone https://github.com/yishawnpeng/modBIN_withXML.git```
+2. Install Python 3.x or later: https://www.python.org/downloads/
+3. Install required libraries: ```pip install -r requirements.txt```
+
+## History
+For more history information, please reference /history/HISTORY.txt
+
+## How to use
+1. Modify target.txt or keep default
+2. Modify Write_Data.bin or keep default
+3. Run ```modbin_withxml.py``` in cmd window
+4. Input which .xml you want to find or press enter to use first finded
+5. Input which .bin you want to modify or press enter to use first finded
+
+> Note: Step 1. and 2. can input manully after step 3.
+
+## Contributing
+If you would like to contribute to this project, please follow these steps:
+ 1. Fork the repository
+ 2. Create a new branch for your feature: ```git checkout -b feature-name```
+ 3. Make changes and commit them: ```git commit -am 'Add some feature'```
+ 4. Push to the branch: ```git push origin feature-name```
+ 5. Submit a pull request
---
+++ modbin_withxml.py
@@ -0,0 +1,134 @@
+#############################
+# UTF-8
+# Shawn.Peng@quantatw.com
+# Can modify binary with location recorded by xml
+############################
+# Work flow :
+# 1.Can change target.txt to modify more step
+#  (1) Default BiosStartInFullRomImage + PcdFlashNvStorageVariableOffset
+#  (2) Rule : one line of feature-name and one line of act 
+#  (3) The act only suppout +/-
+#  (4) If act isn't correct or feature-name cannot find than it will be skiped
+# 2.Find feature-name which user set in {Platform}.xml
+#  (1) Now can find feature-name in tag of first layer and its text is the value we want
+#  (2) Now can find feature-name in value of "name" tag of forth or fifth layers 
+#      and the value of the tag "value" of the same layer is what we want
+#  (3) *FF* User can input {Platfrom} or press enter to use .xml witch first finded
+# 3.Count the value and print it to double check
+# 4.Read write_data.bin which will replace 
+#  (1) *FF* User can input 1 to choice other write_data_factory_default.bin
+#      otherwise default write_data.bin
+#  (2) *FF* User can input manually to modify CT/MAC/ForceNetBoot/counter/timer
+#      otherwise default 11002200...EE00/FF..FF/1/5/2
+#  (3) Format : \x00\x00\..\x07 in one line
+#  (4) *FF* Support other input format
+# 5.Write date to {Platform_version}.bin
+#  (1)Copy {Platform_version}.bin and rename {Platform_version}_ori.bin
+#   progrm will modify in {Platform_version}.bin
+# 
+# 
+# Share point
+# xxx\CMIT_BIOS\
+# GitHub 
+# https://github.com/yishawnpeng/modBIN_withXML
+#
+#############################
+# NOTICE!! : in xml, <PCD> Block Skip
+#############################
+### Get and Count location
+import xml.etree.ElementTree as ET
+import shutil
+import sys
+## Get target
+TargetFileName = "target.txt"
+XMLFileName = "U21.xml"
+#maybe user input filename
+targetName =[]
+targetAct=[]
+with open(TargetFileName, "r") as targetFile :
+    print("Opened Target name : " + TargetFileName )
+    for i in targetFile.readlines() :
+        if i[0:3] != "act":
+            #maybe at end don't have \n
+            targetName.append(i[:-1] if i[-1]=="\n" else i)
+        else :
+            targetAct.append(i[-2])       
+## Get xml info
+tree = ET.parse(XMLFileName)
+print("Opened XML name : " + XMLFileName )
+root = tree.getroot()
+targetValue=[]
+for i in targetName :
+    try:
+        temp = root.find(i).text
+        targetValue.append(temp)
+    except Exception as maybeInSomones_AddressOffset :
+        finded = False
+        for j in root :
+            for k in j :
+                if k.tag == "Address" and not finded :
+                    #print(k ,"k yes")
+                    if k.find("Offset").find("Name").text == i:
+                        #print(k.find("Offset").find("Value").text)
+                        targetValue.append(k.find("Offset").find("Value").text)
+                        finded = True
+                elif not finded : # more deep like PeiA / DxeA
+                    for l in k :
+                        if l.tag == "Address" :
+                            if l.find("Offset").find("Name").text == i :
+                                targetValue.append(l.find("Offset").find("Value").text)
+                                finded = True
+                                break
+                else : # can not find this name
+                    print("Can not find this name : " + i )
+                    print("Skip it !! ")
+                    targetAct.pop(targetName.index(i)-1)
+                if finded :
+                    break
+            if finded :
+                break
+## Count location
+targetValueINT=[int(i, 16) for i in targetValue]
+location = targetValueINT[0]
+totalCount = []
+totalCount.append(targetValueINT[0])
+if len(targetValueINT) >= 2 :
+    for i in range(1, len(targetValueINT)) :
+        if targetAct[i-1] == "+" :
+            location += targetValueINT[i]
+            totalCount.append(targetAct[i-1])
+        elif targetAct[i-1] == "-" :
+            location -= targetValueINT[i]
+        else :
+            print("Don't supoort act : " + targetAct[i-1] + " !! Only +/- !!" )
+            print("Skip this act and value : " + targetAct[i-1] + ","+ targetValueINT[i])
+            break
+else :
+    location = None
+    print("Don't have enough number !!")
+    print("EXIT !!!")
+    sys.exit()
+
+#print(targetName)
+#print(targetValue)
+#print(targetValueINT)
+#print(targetAct)
+print("Taget location at :" , hex(location))
+
+### Mod bin
+BinaryFileName = "U21_892627_32.bin"
+WriteDataFileName = "Write_Data.bin"
+#maybe user input BinaryFileName
+with open(WriteDataFileName, "rb") as writeData :
+    print("Opened Input-Data name : " + WriteDataFileName )
+    data = writeData.read().split()    
+
+shutil.copy2(BinaryFileName, BinaryFileName[:-4]+"_ori"+BinaryFileName[-4:])
+print("Created _ori.bin ")
+with open(BinaryFileName, "rb+") as binaryFile :
+    print("Opened Binary name : " + BinaryFileName )
+    binaryFile.seek(location)
+    #print(BinaryFile.read(1))
+    #BinaryFile.seek(location)
+    for i in data :
+        binaryFile.write(i)
